1cf6ce7be62e38b56e59f72d259cc5632cbb659b
Chozabu <chozabu@gmail.com>
2018-09-13 01:08:44 +0100

    notes on change

--- Source/City/BaseLibrary.cpp
+++ Source/City/BaseLibrary.cpp
@@ -204,7 +204,8 @@ FVector getProperIntersection(FVector p1, FVector p2, FVector p3, FVector p4) {
 	if (res.X == 0.0f) {
 		res = intersection(p1 + otherTangent * backLen, p1 - otherTangent * backLen, p3, p4);
 		if (res.X == 0.0f) {
-			FVector otherTangent2 = FRotator(0, 180, 0).RotateVector(otherTangent);
+			FVector otherTangent2 = FRotator(0, 180, 0).RotateVector(otherTangent); // prevent passing otherTangent2 to its-self before init
+			//it should perhaps be rotated only 90, or be a new tanged based on p3 and p4?
 			res = intersection(p1, p2, p3 - otherTangent2*backLen, p3 + otherTangent2*backLen);
 			if (res.X == 0.0f) {
 				res = intersection(p1, p2, p4 - otherTangent2*backLen, p4 + otherTangent2*backLen);
